066b5382a7a5660191aabeb1a3e6d9c05575928d
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2014-10-23 07:12:33 +0900

    細かく修正した所をコミット

--- KokubanForSwift/ViewController.swift
+++ KokubanForSwift/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var chook1: UIButton!
+    @IBOutlet weak var elaser: UIButton!
+    @IBOutlet weak var chook2: UIButton!
+    @IBOutlet weak var chook3: UIButton!
+    @IBOutlet weak var chook4: UIButton!
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
