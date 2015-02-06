fb7e47189aec8b9fd13de23a2fc5ba9c4163bb8b
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-02-06 12:43:38 +0900

    念のため、この時点で一旦コミット

--- KokubanForSwift/ViewController.swift
+++ KokubanForSwift/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var chook2: UIButton!
     @IBOutlet weak var chook3: UIButton!
     @IBOutlet weak var chook4: UIButton!
+    @IBOutlet weak var chook5: UIButton!
     @IBOutlet weak var tab: UIButton!
     @IBOutlet weak var aImageView: UIImageView!
     @IBOutlet var pastDrawingView: DrawingView!
@@ -27,6 +28,11 @@ class ViewController: UIViewController {
     var penGreen:CGFloat
     var penAlpha:CGFloat
     var select_chook:Int
+    var kokubanMode:Bool
+    var billingMode:Bool
+    var aryStroke:NSMutableArray
+    var sourceType:UIImagePickerControllerSourceType
+    
     
 
     required init(coder aDecoder: NSCoder) {
@@ -52,29 +58,200 @@ class ViewController: UIViewController {
     }
     
     @IBAction func chook1(sender: AnyObject) {
+        Allstand(1)
+        setPenColor(index: 0)
+        kokubanMode = true
     }
     @IBAction func chook2(sender: AnyObject) {
+        Allstand(2)
+        setPenColor(index: 1)
+        kokubanMode = true
     }
     @IBAction func chook3(sender: AnyObject) {
+        Allstand(3)
+        setPenColor(index: 2)
+        kokubanMode = true
     }
     @IBAction func chook4(sender: AnyObject) {
+        Allstand(4)
+        setPenColor(index: 3)
+        kokubanMode = true
     }
     @IBAction func chook5(sender: AnyObject) {
+        Allstand(5)
+        setPenColor(index: 4)
+        kokubanMode = true
+        
     }
-    @IBAction func elase(sender: AnyObject) {
+    
+    func elase() {
+        Allstand(10)
+        setPenColor(index: 5)
+        kokubanMode = true
+        
+    }
+    @IBAction func showEraseSheet(sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "どうする？？", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+        let okButton = UIAlertAction(title: "一つ前に戻る", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+            println("Ok Selected")
+            self.kokubanMode = true
+            if self.aryStroke.count > 0 {
+                self.aryStroke.removeLastObject()
+                self.pastDrawingView.aryData = self.aryStroke
+                self.pastDrawingView.setNeedsDisplay()
+            }
+            
+        }
+        let cancelButton = UIAlertAction(title: "全消し", style: UIAlertActionStyle.Cancel) { (cancelSelected) -> Void in
+            println("Cancel Selected")
+            
+            self.kokubanMode = true
+            if self.aryStroke.count > 0 {
+                self.aryStroke.removeAllObjects()
+                self.pastDrawingView.aryData = self.aryStroke
+                self.pastDrawingView.setNeedsDisplay()
+            }
+        }
+        alert.addAction(okButton)
+        alert.addAction(cancelButton)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    @IBAction func showActionSheet(sender: AnyObject) {
+        if !self.billingMode {
+            let alert = UIAlertController(title: "どうする？？", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+            let button1 = UIAlertAction(title: "写真を選ぶ", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+                
+            }
+            let button2 = UIAlertAction(title: "ライブラリに保存", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+            let button3 = UIAlertAction(title: "黒板を表示", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+            let button4 = UIAlertAction(title: "ひまわり", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+            let button5 = UIAlertAction(title: "チョークを使う", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+            let button6 = UIAlertAction(title: "線の太さを変える", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+            let button7 = UIAlertAction(title: "黒板の有料版を購入", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+    
+            alert.addAction(button1)
+            alert.addAction(button2)
+            alert.addAction(button3)
+            alert.addAction(button4)
+            alert.addAction(button5)
+            alert.addAction(button6)
+            alert.addAction(button7)
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+            
+        }else{
+            let alert = UIAlertController(title: "どうする？？", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+            let button1 = UIAlertAction(title: "写真を選ぶ", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+            let button2 = UIAlertAction(title: "ライブラリに保存", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+            let button3 = UIAlertAction(title: "黒板を表示", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+            let button4 = UIAlertAction(title: "ひまわり", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+            let button5 = UIAlertAction(title: "チョークを使う", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+            let button6 = UIAlertAction(title: "線の太さを変える", style: UIAlertActionStyle.Default) { (okSelected) -> Void in
+                
+            }
+            
+            
+            alert.addAction(button1)
+            alert.addAction(button2)
+            alert.addAction(button3)
+            alert.addAction(button4)
+            alert.addAction(button5)
+            alert.addAction(button6)
+            
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func selectLibraly() {
+        self.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        if UIImagePickerController.isSourceTypeAvailable(self.sourceType) {
+            
+        }
     }
     
     func Allstand(stand:Int){
         
         switch select_chook{
             case 1:
+                self.chook1.transform = CGAffineTransformMakeRotation(0)
             break
             case 2:
+                self.chook2.transform = CGAffineTransformMakeRotation(0)
+            break
+            case 3:
+                self.chook3.transform = CGAffineTransformMakeRotation(0)
+            break
+            case 4:
+                self.chook4.transform = CGAffineTransformMakeRotation(0)
+            break
+            case 5:
+                self.chook5.transform = CGAffineTransformMakeRotation(0)
+            break
+            case 6:
+                self.tab.enabled		= true
             break
             default:
             break;
             
         }
+        switch select_chook{
+            case 1:
+                self.chook1.transform = CGAffineTransformMakeRotation(10)
+            break
+            case 2:
+                self.chook2.transform = CGAffineTransformMakeRotation(10)
+            break
+            case 3:
+                self.chook3.transform = CGAffineTransformMakeRotation(10)
+            break
+            case 4:
+                self.chook4.transform = CGAffineTransformMakeRotation(10)
+            break
+            case 5:
+                self.chook5.transform = CGAffineTransformMakeRotation(10)
+            break
+            case 6:
+                self.tab.enabled		= false
+            break
+            default:
+            break;
+            
+        }
+        select_chook = stand
+        
+    }
+    
+    func store() {
         
     }
     
