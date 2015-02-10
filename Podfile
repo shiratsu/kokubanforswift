3f5611cbb247c4d00d4657a7cdf6493b4269c653
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-02-10 17:50:45 +0900

    いろいろ修正

---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :ios, '7.0'
+pod 'Google-Mobile-Ads-SDK', '~> 6.10'
+pod 'SVProgressHUD', :head
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Google-Mobile-Ads-SDK
+#define COCOAPODS_POD_AVAILABLE_Google_Mobile_Ads_SDK
+#define COCOAPODS_VERSION_MAJOR_Google_Mobile_Ads_SDK 6
+#define COCOAPODS_VERSION_MINOR_Google_Mobile_Ads_SDK 12
+#define COCOAPODS_VERSION_PATCH_Google_Mobile_Ads_SDK 0
+
+// SVProgressHUD
+#define COCOAPODS_POD_AVAILABLE_SVProgressHUD
+#define COCOAPODS_VERSION_MAJOR_SVProgressHUD 1
+#define COCOAPODS_VERSION_MINOR_SVProgressHUD 0
+#define COCOAPODS_VERSION_PATCH_SVProgressHUD 0
+
